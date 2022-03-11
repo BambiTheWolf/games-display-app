f8e2aa8fa8a325658fa55771c37164883a5c8dd6
Michael Redruello <redruellomichael@gmail.com>
2022-03-11 17:29:50 +0100

    Game of Life added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import HomePage from './components/Homepage';
+import GameOfLife from './components/GameOfLife';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/GameOfLife' element={<GameOfLife />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
